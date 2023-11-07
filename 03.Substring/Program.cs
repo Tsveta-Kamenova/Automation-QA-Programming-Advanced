dd804513476c68c152889a414bb14f37284ed32f
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-07 18:37:14 +0200

    strings

---
+++ 03.Substring/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string key = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int index = text.IndexOf(key);
+
+            while (index != -1)
+            {
+                text = text.Remove(index, key.Length);
+                index = text.IndexOf(key);
+            }
+
+            Console.WriteLine(text);
+
+            //    string stringToBeRemoved = Console.ReadLine() ?? "ice";
+            //    string stringInput = Console.ReadLine() ?? "kicegiciceeb";
+
+
+            //    while (true)
+            //    {
+            //        if (!stringInput.Contains(stringToBeRemoved))
+            //        {
+            //            break;
+            //        }
+            //        stringInput = stringInput.Replace(stringToBeRemoved, "");
+            //    }
+
+            //    Console.WriteLine(stringInput);
+        }
+    }
+}
---
+++ 04.TextFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banWords = (Console.ReadLine()?? "a").Split(", ");
+            string textToBeChanged = Console.ReadLine() ?? "abc";
+
+            foreach (var banWord in banWords)
+            {
+                if (textToBeChanged.Contains(banWord))
+                {
+                    textToBeChanged = textToBeChanged.Replace(banWord, new string('*', banWord.Length));
+                }
+            }
+            Console.WriteLine(textToBeChanged);
+        }
+    }
+}

5a2469e205e3d14fc389c9f9c998d41addcaed8b
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-10 17:03:26 +0200

    strings

--- 01.ReverseStrings/Program.cs
+++ 01.ReverseStrings/Program.cs
@@ -1,54 +1,94 @@
-﻿namespace _01.ReverseStrings;
+﻿using System.Linq;
+
+namespace _01.ReverseStrings;
 class Program
 {
-//    static void Main(string[] args)
-//    {
-//        string currentInputString = Console.ReadLine() ?? "abc";
+    //    static void Main(string[] args)
+    //    {
+    //        string currentInputString = Console.ReadLine() ?? "abc";
 
-//        while (currentInputString != "end")
-//        {
-//            string currentInputStringReversed = Reverse(currentInputString);
-//            Console.WriteLine($"{currentInputString} = {currentInputStringReversed}");
-//            currentInputString = Console.ReadLine() ?? "abc";
-//        }
-//    }
+    //        while (currentInputString != "end")
+    //        {
+    //            string currentInputStringReversed = Reverse(currentInputString);
+    //            Console.WriteLine($"{currentInputString} = {currentInputStringReversed}");
+    //            currentInputString = Console.ReadLine() ?? "abc";
+    //        }
+    //    }
 
-//    public static string Reverse(string s)
-//    {
-//        char[] charArray = s.ToCharArray();
-//        Array.Reverse(charArray);
-//        return new string(charArray);
-//    }
-//}
+    //    public static string Reverse(string s)
+    //    {
+    //        char[] charArray = s.ToCharArray();
+    //        Array.Reverse(charArray);
+    //        return new string(charArray);
+    //    }
+    //}
+
+    //static void Main()
+    //{
+    //    List<string> reversedWords = new List<string>();
+
+    //    while (true)
+    //    {
+    //        string input = Console.ReadLine();
+
+    //        if (input == "end")
+    //            break;
+
+    //        string reversedWord = ReverseString(input);
+    //        reversedWords.Add($"{input} = {reversedWord}");
+    //    }
+
+    //    foreach (string pair in reversedWords)
+    //    {
+    //        Console.WriteLine(pair);
+    //    }
+    //}
+
+    //static string ReverseString(string input)
+    //{
+    //    string reversedWord = "";
+    //    for (int i = input.Length - 1; i >= 0; i--)
+    //    {
+    //        reversedWord += input[i];
+    //    }
+    //    return reversedWord;
+    //}
+
+    //static void Main()
+    //{
+    //    string word = Console.ReadLine();
+
+    //    while (word != "end")
+    //    {
+    //        string reversedWord = "";
+
+    //        for (int i = word.Length -1; i >= 0; i--)
+    //        {
+    //            reversedWord += word[i];
+    //        }
+
+    //        Console.WriteLine($"{word} = {reversedWord}");
+
+    //        word = Console.ReadLine();
+    //    }
+    //}
 
     static void Main()
     {
-        List<string> reversedWords = new List<string>();
+        string word = Console.ReadLine();
 
-        while (true)
+        while (word != "end")
         {
-            string input = Console.ReadLine();
 
-            if (input == "end")
-                break;
+            char[] wordsAsCharArray = word.ToCharArray();
+            Array.Reverse(wordsAsCharArray);
 
-            string reversedWord = ReverseString(input);
-            reversedWords.Add($"{input} = {reversedWord}");
-        }
+            string reversedWord = string.Join("", wordsAsCharArray);
 
-        foreach (string pair in reversedWords)
-        {
-            Console.WriteLine(pair);
+            Console.WriteLine($"{word} = {reversedWord}");
+
+            word = Console.ReadLine();
         }
     }
 
-    static string ReverseString(string input)
-    {
-        string reversedWord = "";
-        for (int i = input.Length - 1; i >= 0; i--)
-        {
-            reversedWord += input[i];
-        }
-        return reversedWord;
-    }
 }
--- 02.RepeatStrings/Program.cs
+++ 02.RepeatStrings/Program.cs
@@ -1,23 +1,53 @@
-﻿namespace _02.RepeatStrings
+﻿using System.Text;
+
+namespace _02.RepeatStrings
 {
     internal class Program
     {
+        //static void Main(string[] args)
+        //{
+        //    string[] words = Console.ReadLine().Split();
+
+        //    StringBuilder result = new();
+
+        //    for (int i = 0; i < words.Length; i++)
+        //    {
+        //        string currentword = words[i];
+
+        //        for (int j = 0; j < currentword.Length; j++)
+        //        {
+        //            result.Append(currentword);
+        //        }
+        //    }
+        //    Console.WriteLine(result);
+        //}
+
+
+        //static void Main(string[] args)
+        //{
+        //    string[] words = Console.ReadLine().Split();
+
+        //    string result = "";
+
+        //    foreach (string word in words)
+        //    {
+        //        int repeatTimes = word.Length;
+
+        //        for (int i = 0; i < repeatTimes; i++)
+        //        {
+        //            result += word;
+        //        }
+        //    }
+        //    Console.WriteLine(result);
+        //}
+
         static void Main(string[] args)
         {
-            string[] words = Console.ReadLine().Split();
+            string words = string.Concat(Console.ReadLine().Split(" ").Select(x => string.Concat(Enumerable.Repeat(x, x.Length))));
 
-            string result = "";
-
-            foreach (string word in words)
-            {
-                int repeatTimes = word.Length;
-
-                for (int i = 0; i < repeatTimes; i++)
-                {
-                    result += word;
-                }
-            }
-            Console.WriteLine(result);
+            Console.WriteLine(words);
         }
+
+
     }
 }

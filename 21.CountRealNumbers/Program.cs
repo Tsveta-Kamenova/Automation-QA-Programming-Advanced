e7f86e917c7ac0f64d8493ff294fa33ea5be11cc
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-15 04:50:12 +0200

    dictionaries

---
+++ 21.CountRealNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _21.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            SortedDictionary<int, int> numbersFrequency = new();
+
+            foreach (int number in numbers)
+            {
+                if (numbersFrequency.ContainsKey(number)) numbersFrequency[number]++;
+                else
+                {
+                    numbersFrequency[number] = 1;
+                }
+            }
+            foreach (KeyValuePair<int, int> pair in numbersFrequency)
+            {
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+
+
+        }
+    }
+}

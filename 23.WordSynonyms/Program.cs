d406c88d00b6bc03be090df31259cde1ffae7e86
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-16 19:21:14 +0200

    and LINQ

---
+++ 23.WordSynonyms/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _23.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPairs = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> synonymsDictionary = new();
+
+            for (int i = 0; i < numberOfPairs; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (synonymsDictionary.ContainsKey(word))
+                {
+                    synonymsDictionary[word].Add(synonym);
+                }
+                else
+                {
+                    synonymsDictionary.Add(word, new List<string>() { synonym });
+                }
+            }
+
+            foreach (KeyValuePair<string, List<string>> pair in synonymsDictionary)
+            {
+                Console.WriteLine($"{pair.Key} - {string.Join(", ", pair.Value)}");
+            }
+        }
+    }
+}
---
+++ 24.WordFilter/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace _24.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ").ToArray();
+            string[] result = words.Where(X => X.Length % 2 == 0).ToArray();
+
+            foreach (string word in result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}

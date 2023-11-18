00d2fec62cfcd633e63e36c032eacb31b1ab0977
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-18 22:22:16 +0200

    Dictionaries

---
+++ 33.Orders/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _33.Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Dictionary<string, List<decimal>> productPricesDict = new();
+
+            while (input != "buy")
+            {
+
+                string[] inputArray = input.Split();
+                string product = inputArray[0];
+                decimal price = decimal.Parse(inputArray[1]);
+                decimal quantity = decimal.Parse(inputArray[2]);
+
+                if (!productPricesDict.ContainsKey(product))
+                {
+                    productPricesDict.Add(product, new() { price, quantity});
+
+                    //productPricesDict.Add(product, new List<double>());
+                    //productPricesDict[product].Add(price);
+                    //productPricesDict[product].Add(quantity);
+                }
+                else
+                {
+                    productPricesDict[product][0] = price;
+                    productPricesDict[product][1] += quantity;
+                }
+
+                input = Console.ReadLine();
+            }
+            
+            foreach (KeyValuePair<string, List<decimal>> currentProduct in productPricesDict)
+            {
+                string currentProductName = currentProduct.Key;
+                decimal currentPrice = currentProduct.Value[0];
+                decimal currentQuantity = currentProduct.Value[1];
+
+                decimal totalPrice = currentPrice * currentQuantity;
+
+                Console.WriteLine($"{currentProductName} -> {totalPrice:f2}");
+            }
+        }
+    }
+}
---
+++ 34.SoftUniParking/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _34.SoftUniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> employeePlateNumber = new();
+
+            int employees = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < employees; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+
+                string command = input[0];
+                string employeeName = input[1];
+
+
+                if (command == "register")
+                {
+                    string plateNumber = input[2];
+
+                    if (employeePlateNumber.ContainsKey(employeeName))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {employeePlateNumber[employeeName]}");
+                    }
+                    else
+                    {
+                        employeePlateNumber[employeeName] = plateNumber;
+                        Console.WriteLine($"{employeeName} registered {plateNumber} successfully");
+                    }
+                }
+                else
+                {
+                    if (employeePlateNumber.ContainsKey(employeeName))
+                    {
+                        Console.WriteLine($"{employeeName} unregistered successfully");
+                    }
+                    else 
+                    {
+                        Console.WriteLine($"ERROR: user {employeeName} not found");
+                    }
+                }
+            }
+
+            foreach (var currentEmployee in employeePlateNumber)
+            {
+                Console.WriteLine(currentEmployee.Key + " => " + currentEmployee.Value);
+            }
+        }
+    }
+}

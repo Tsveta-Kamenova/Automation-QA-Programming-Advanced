23df16cd18118b113378f1f1026ff89b5f76064f
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-19 23:37:04 +0200

    unit testing of dictionaries

---
+++ 06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/CountCharactersTests.cs
@@ -0,0 +1,132 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.Tests;
+
+public class CountCharactersTests
+{
+    [Test]
+    public void Test_Count_WithEmptyList_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithNoCharacters_ShouldReturnEmptyString()
+    {
+        // Arrange
+        List<string> input = new() { "", "", "" };
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleCharacter_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "a", "aaaa", "aaa"};
+
+        // Act
+        string result = CountCharacters.Count(input);
+        string expectedResult = "a -> 8";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Count_WithOnlyOneCharacter_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "a"};
+
+        // Act
+        string result = CountCharacters.Count(input);
+        string expectedResult = "a -> 1";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "abcbaaaa", "asd", "b" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("a -> 6");
+        sb.AppendLine("b -> 3");
+        sb.AppendLine("c -> 1");
+        sb.AppendLine("s -> 1");
+        sb.AppendLine("d -> 1");
+
+        string expectedResult = sb.ToString().Trim();
+
+        // Act
+        string result = CountCharacters.Count(input);
+        //string expectedResult = "a -> 6\r\nb -> 3\r\nc -> 1\r\ns -> 1\r\nd -> 1";
+
+        // Assert
+        Assert.That(result , Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Count_WithSpecialCharacters_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "$@aa#", "@" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("$ -> 1");
+        sb.AppendLine("@ -> 2");
+        sb.AppendLine("a -> 2");
+        sb.AppendLine("# -> 1");
+
+        string expectedResult = sb.ToString().Trim();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+    [Test]
+    public void Test_Count_WithUpperCaseAndNumbers_ShouldReturnCountString()
+    {
+        // Arrange
+        List<string> input = new() { "$@aA#", "@2 1" };
+
+        StringBuilder sb = new();
+        sb.AppendLine("$ -> 1");
+        sb.AppendLine("@ -> 2");
+        sb.AppendLine("a -> 1");
+        sb.AppendLine("A -> 1");
+        sb.AppendLine("# -> 1");
+        sb.AppendLine("2 -> 1");
+        sb.AppendLine("  -> 1");
+        sb.AppendLine("1 -> 1");
+
+        string expectedResult = sb.ToString().Trim();
+
+        // Act
+        string result = CountCharacters.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}
---
+++ 06-Resources-Unit-Testing-Exercise-Dictionaries/TestApp.Tests/CountRealNumbersTests.cs
@@ -0,0 +1,128 @@
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestApp.Tests;
+
+public class CountRealNumbersTests
+{
+    [Test]
+    public void Test_Count_WithEmptyArray_ShouldReturnEmptyString()
+    {
+        // Arrange
+        //int[] input = new int[0];
+        //int[] input = Array.Empty<int>();
+        int[] input = { };
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Test_Count_WithSingleNumber_ShouldReturnCountString()
+    {
+        // Arrange
+        int[] input = { 5 };
+
+        StringBuilder sb = new();
+        sb.AppendLine("5 -> 1");
+
+
+        string expectedResult = sb.ToString().Trim();
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Count_WithMultipleNumbers_ShouldReturnCountString()
+    {
+        // Arrange
+        int[] input = { 2, 1, 1, 4, 4, 4, 4, 1 };
+
+        StringBuilder sb = new();
+        sb.AppendLine("1 -> 3");
+        sb.AppendLine("2 -> 1");
+        sb.AppendLine("4 -> 4");
+    
+        string expectedResult = sb.ToString().Trim();
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Count_WithNegativeNumbers_ShouldReturnCountString()
+    {
+        // Arrange
+        int[] input = { 3, 2, 1, -2, 1, -4, -4, -4 };
+
+
+        StringBuilder sb = new();
+        sb.AppendLine("-4 -> 3");
+        sb.AppendLine("-2 -> 1");
+        sb.AppendLine("1 -> 2");
+        sb.AppendLine("2 -> 1");
+        sb.AppendLine("3 -> 1");
+
+
+        string expectedResult = sb.ToString().Trim();
+
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Count_WithZero_ShouldReturnCountString()
+    {
+        // Arrange
+        int[] input = { 0, -2, 1, 4, 0, 0 };
+
+
+        StringBuilder sb = new();
+        sb.AppendLine("-2 -> 1");
+        sb.AppendLine("0 -> 3");
+        sb.AppendLine("1 -> 1");
+        sb.AppendLine("4 -> 1");
+
+        string expectedResult = sb.ToString().Trim();
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+
+    [Test]
+    public void Test_Count_WithSingleZero_ShouldReturnCountString()
+    {
+        // Arrange
+        int[] input = { 0, 0, 0, 0, 0};
+
+
+        StringBuilder sb = new();
+        sb.AppendLine("0 -> 5");
+
+
+        string expectedResult = sb.ToString().Trim();
+        // Act
+        string result = CountRealNumbers.Count(input);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expectedResult));
+    }
+}

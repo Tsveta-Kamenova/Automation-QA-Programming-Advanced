86dfdbe6a3cfb5e48bd7eb74ab5408dffe924c20
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-21 23:36:31 +0200

    objects and classes

---
+++ 42.Students/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _42.Students
+{
+    internal class Program
+    {
+        public class Student
+        {
+            public string FirstName { get; set; }
+
+            public string LastName { get; set; }
+
+            public int Age { get; set; }
+            
+            public string HomeTown { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>();
+
+            string[] studentsInputInformation = Console.ReadLine().Split().ToArray();
+
+            string checkEnd = studentsInputInformation[0];
+
+            while (checkEnd != "end")
+            {
+                string studentFirstName = studentsInputInformation[0];
+                string studentLastName = studentsInputInformation[1];
+                int studentAge = int.Parse(studentsInputInformation[2]);
+                string studentHomeTown = studentsInputInformation[3];
+
+                Student student = new Student
+                {
+                    FirstName = studentFirstName,
+                    LastName = studentLastName,
+                    Age = studentAge,
+                    HomeTown = studentHomeTown,
+                };
+
+                students.Add(student);
+
+                studentsInputInformation = Console.ReadLine().Split().ToArray();
+
+                checkEnd = studentsInputInformation[0];
+            }
+            
+            string cityName = Console.ReadLine();
+
+            List<Student> filteredStudents = students
+                                            .Where(x => x.HomeTown == cityName)
+                                            .ToList();
+
+            foreach (var currentFilteredStudent in filteredStudents)
+            {
+                Console.WriteLine($"{currentFilteredStudent.FirstName} {currentFilteredStudent.LastName} is {currentFilteredStudent.Age} years old.");
+            }
+        }
+    }
+}
---
+++ 43.StoreBoxes/Program.cs
@@ -0,0 +1,76 @@
+﻿namespace _43.StoreBoxes
+{
+    internal class Program
+    {
+
+        public class Item
+        {
+            public Item(string name, decimal price)
+            {
+                Name = name;
+                Price = price;
+            }
+            public string Name { get; set; }
+
+            public decimal Price { get; set; }
+
+        }
+
+        public class Box
+        {
+            public Box(string serialNumber, Item itemName, int itemQuantity)
+            {
+                SerialNumber = serialNumber;
+                ItemQuantity = itemQuantity;
+                Item = itemName;
+                PriceForABox = Item.Price * itemQuantity;
+            }
+
+            public string SerialNumber { get; set; }
+
+            public Item Item { get; set; }
+
+            public int ItemQuantity { get; set; }
+
+            public decimal PriceForABox { get; set; }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            List<Box> boxes = new List<Box>();  
+
+            while (command != "end")
+            {
+                string[] data = command.Split().ToArray();
+
+                string currentSerialNumber = data[0];
+                string currentItemName = data[1];
+                int currentQuantity = int.Parse(data[2]);
+                decimal currentPrice = decimal.Parse(data[3]);
+
+                Item currentItem = new Item(currentItemName, currentPrice);
+
+                Box currentBox = new Box(currentSerialNumber, currentItem, currentQuantity);
+
+                boxes.Add(currentBox);
+
+                command = Console.ReadLine();
+            }
+
+            boxes = boxes
+                    .OrderByDescending(box => box.PriceForABox)
+                    .ToList();
+
+            foreach (var box in boxes)
+            {
+                Console.WriteLine($"{box.SerialNumber}");
+                Console.WriteLine($"-- {box.Item.Name} – ${box.Item.Price:f2}: {box.ItemQuantity}");
+                Console.WriteLine($"-- ${box.PriceForABox}");
+
+            }
+        }
+    }
+}

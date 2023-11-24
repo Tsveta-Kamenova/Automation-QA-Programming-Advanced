8851464bd20743d1f672841af98b2dfcf3a251bb
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-24 23:06:41 +0200

    defining classes

--- DefiningClasses/CarManufacturer/Car.cs
+++ DefiningClasses/CarManufacturer/Car.cs
@@ -38,5 +38,37 @@ namespace CarManufacturer
                     $"Fuel: {this.FuelQuantity:F2}";
         }
 
+        public Car()
+        {
+            this.Make = "VW";
+
+            this.Model = "Golf";
+
+            this.Year = 2025;
+
+            this.FuelQuantity = 200;
+
+            this.FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year)
+        :this()
+        {
+            this.Make = make;
+
+            this.Model = model;
+
+            this.Year = year;
+        }
+
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption)
+        : this(make, model, year)
+        {
+
+            this.FuelQuantity = fuelQuantity;
+
+            this.FuelConsumption = fuelConsumption;
+        }
+
     }
 }
--- DefiningClasses/CarManufacturer/StartUp.cs
+++ DefiningClasses/CarManufacturer/StartUp.cs
@@ -8,19 +8,32 @@ namespace CarManufacturer
 {
     public class StartUp
     {
-        static void Main()
+        //static void Main()
+        //{
+        //    Car car = new Car();
+
+        //    car.Make = "VW";
+        //    car.Model = "MK3";
+        //    car.Year = 1992;
+        //    car.FuelQuantity = 200;
+        //    car.FuelConsumption = 200;
+        //    car.Drive(2000);
+
+        //    Console.WriteLine(car.WhoAmI());
+        //    //Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+        //}
+
+        public static void Main(string[] args)
         {
-            Car car = new Car();
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsumption = double.Parse(Console.ReadLine());
 
-            car.Make = "VW";
-            car.Model = "MK3";
-            car.Year = 1992;
-            car.FuelQuantity = 200;
-            car.FuelConsumption = 200;
-            car.Drive(2000);
-
-            Console.WriteLine(car.WhoAmI());
-            //Console.WriteLine($"Make: {car.Make}\nModel: {car.Model}\nYear: {car.Year}");
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsumption);
         }
     }
 }

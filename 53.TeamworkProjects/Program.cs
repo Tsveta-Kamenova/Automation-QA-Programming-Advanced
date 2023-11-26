bbf6afc5c564a8a882a24e33b7b5f28c4c5a066e
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-27 00:03:44 +0200

    objects and classes

--- 53.TeamworkProjects/Program.cs
+++ 53.TeamworkProjects/Program.cs
@@ -4,7 +4,66 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numberOfTeams = int.Parse(Console.ReadLine());
+
+            List<Team> teams = new();
+
+            for (int i = 0; i < numberOfTeams; i++)
+            {
+                string[] registerInput = Console.ReadLine().Split("-").ToArray();
+
+                string firstUserName = registerInput[0];
+                string firstTeamName = registerInput[1];
+
+                foreach (Team team in teams)
+                {
+                    if (team.UserCreator == firstUserName)
+                    {
+                        Console.WriteLine($"{firstUserName} cannot create another team!");
+                        continue;
+                    }
+
+                    if (team.TeamName.Contains(firstTeamName))
+                    {
+                        Console.WriteLine($"Team {firstTeamName} was already created!");
+                        continue;
+                    }
+
+                    Team currentTeam = new Team(firstUserName, firstTeamName);
+
+                    teams.Add(currentTeam);
+                }
+            }
+
+            string input = Console.ReadLine();
+
+            while(input != "end of assignment")
+            {
+                string[] joinInput = input.Split("->").ToArray();
+
+                string memberWannaBe = joinInput[0];
+                string joinTeam = joinInput[1];
+
+                foreach (Team team in teams)
+                {
+                    if (team.UserCreator == memberWannaBe)
+                    {
+                        Console.WriteLine($"Member {memberWannaBe} cannot join team {joinTeam}!");
+                        continue;
+                    }
+
+                    if (team.TeamName == joinTeam && team.UserCreator != memberWannaBe)
+                    {
+                        Team currentTeam = new Team(memberWannaBe, joinTeam);
+
+                        teams.Add(currentTeam);
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+
+
         }
     }
 }

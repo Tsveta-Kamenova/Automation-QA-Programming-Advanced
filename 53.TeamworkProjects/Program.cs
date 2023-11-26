7daf60f1c2fa71f4b07eac4c9d1dbe8ba272f271
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-11-26 23:59:53 +0200

    objects and classes

---
+++ 53.TeamworkProjects/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _53.TeamworkProjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

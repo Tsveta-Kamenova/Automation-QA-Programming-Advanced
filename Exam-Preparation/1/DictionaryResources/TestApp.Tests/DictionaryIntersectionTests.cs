77030803aefaa05cb533cd3e520f6b03c56cf139
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-12-12 17:47:15 +0200

    exam prep

--- Exam-Preparation/1/DictionaryResources/TestApp.Tests/DictionaryIntersectionTests.cs
+++ Exam-Preparation/1/DictionaryResources/TestApp.Tests/DictionaryIntersectionTests.cs
@@ -10,25 +10,101 @@ public class DictionaryIntersectionTests
     [Test]
     public void Test_Intersect_TwoEmptyDictionaries_ReturnsEmptyDictionary()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> firstDictionary = new();
+        Dictionary<string, int> secondDictionary = new();
+
+        // Act 
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+
+        // Assert
+        Assert.That(result.Count, Is.EqualTo(0));
+        //Assert.That(result, Has.Count.EqualTo(0));
     }
 
     [Test]
     public void Test_Intersect_OneEmptyDictionaryAndOneNonEmptyDictionary_ReturnsEmptyDictionary()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> firstDictionary = new();
+        Dictionary<string, int> secondDictionary = new()
+        {
+            { "one" , 1 },
+            { "two" , 2 },
+            { "three" , 3 },
+        };
+
+        // Act 
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+
+        // Assert
+        //Assert.That(result.Count, Is.EqualTo(0));
+        //Assert.That(result, Has.Count.EqualTo(0));
+        Assert.IsTrue(result.Count == 0);
     }
 
     [Test]
     public void Test_Intersect_TwoNonEmptyDictionariesWithNoCommonKeys_ReturnsEmptyDictionary()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> firstDictionary = new()
+        {
+            { "five" , 5 },
+            { "six" , 6 },
+            { "seven" , 7 },
+        };
+        Dictionary<string, int> secondDictionary = new()
+        {
+            { "one" , 1 },
+            { "two" , 2 },
+            { "three" , 3 },
+        };
+
+        // Act 
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+
+        // Assert
+        //Assert.That(result.Count, Is.EqualTo(0));
+        //Assert.That(result, Has.Count.EqualTo(0));
+        Assert.IsTrue(result.Count == 0);
     }
 
     [Test]
     public void Test_Intersect_TwoNonEmptyDictionariesWithCommonKeysAndValues_ReturnsIntersectionDictionary()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> firstDictionary = new()
+        {
+            { "five" , 5 },
+            { "six" , 6 },
+            { "seven" , 7 },
+            { "one" , 1 },
+            { "two" , 2 },
+        };
+        Dictionary<string, int> secondDictionary = new()
+        {
+            { "one" , 1 },
+            { "two" , 2 },
+            { "three" , 3 },
+            { "seven" , 7 },
+        };
+
+        Dictionary<string, int> expectedDictionary = new()
+        {
+            { "one" , 1 },
+            { "two" , 2 },
+            { "seven" , 7 },
+        };
+
+        // Act 
+        Dictionary<string, int> result = DictionaryIntersection.Intersect(firstDictionary, secondDictionary);
+
+
+        // Assert
+        Assert.That(expectedDictionary, Is.EqualTo(result));
     }
 
     [Test]
--- Exam-Preparation/1/StringResources/TestApp.Tests/StringRotatorTests.cs
+++ Exam-Preparation/1/StringResources/TestApp.Tests/StringRotatorTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System.Security.Cryptography;
 
 namespace TestApp.Tests;
 
@@ -8,30 +9,75 @@ public class StringRotatorTests
     [Test]
     public void Test_RotateRight_EmptyString_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = string.Empty;
+        int rotations = 189;
+
+        // Act 
+        string result = StringRotator.RotateRight(input, rotations);
+        string expected = string.Empty;
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_RotateRight_RotateByZeroPositions_ReturnsOriginalString()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "Qwerty89";
+        int rotations = 0;
+
+        // Act 
+        string result = StringRotator.RotateRight(input, rotations);
+        string expected = "Qwerty89";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_RotateRight_RotateByPositivePositions_ReturnsRotatedString()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "Qwerty89";
+        int rotations = 3;
+
+        // Act 
+        string result = StringRotator.RotateRight(input, rotations);
+        string expected = "y89Qwert";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_RotateRight_RotateByNegativePositions_ReturnsRotatedString()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "Qwerty89";
+        int rotations = -5;
+
+        // Act 
+        string result = StringRotator.RotateRight(input, rotations);
+        string expected = "rty89Qwe";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_RotateRight_RotateByMorePositionsThanStringLength_ReturnsRotatedString()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "Qwerty89";
+        int rotations = 3 + (input.Length * 2);
+
+        // Act 
+        string result = StringRotator.RotateRight(input, rotations);
+        string expected = "y89Qwert";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }

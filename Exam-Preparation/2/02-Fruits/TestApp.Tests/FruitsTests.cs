09290d0ad1919327b93167aad9cdd62bcc472b15
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-12-14 17:47:44 +0200

    exam prep

--- Exam-Preparation/2/02-Fruits/TestApp.Tests/FruitsTests.cs
+++ Exam-Preparation/2/02-Fruits/TestApp.Tests/FruitsTests.cs
@@ -10,30 +10,97 @@ public class FruitsTests
     [Test]
     public void Test_GetFruitQuantity_FruitExists_ReturnsQuantity()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> fruitsDict = new()
+        {
+            { "apple" , 5 },
+            { "banana" , 6 },
+        };
+
+        string fruitName = "apple";
+        int expected = 5;
+
+        // Act 
+        int result = Fruits.GetFruitQuantity(fruitsDict,fruitName);
+
+
+        // Assert
+        Assert.That(expected, Is.EqualTo(result));
     }
 
     [Test]
     public void Test_GetFruitQuantity_FruitDoesNotExist_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> fruitsDict = new()
+        {
+            { "apple" , 5 },
+            { "banana" , 6 },
+        };
+
+        string fruitName = "orange";
+        int expected = 0;
+
+        // Act 
+        int result = Fruits.GetFruitQuantity(fruitsDict, fruitName);
+
+
+        // Assert
+        Assert.That(expected, Is.EqualTo(result));
     }
 
     [Test]
     public void Test_GetFruitQuantity_EmptyDictionary_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> fruitsDict = new();
+
+        string fruitName = "orange";
+        int expected = 0;
+
+        // Act 
+        int result = Fruits.GetFruitQuantity(fruitsDict, fruitName);
+
+
+        // Assert
+        Assert.That(expected, Is.EqualTo(result));
     }
 
     [Test]
     public void Test_GetFruitQuantity_NullDictionary_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> fruitsDict = null;
+
+        string fruitName = "banana";
+        int expected = 0;
+
+        // Act 
+        int result = Fruits.GetFruitQuantity(fruitsDict, fruitName);
+
+
+        // Assert
+        Assert.That(expected, Is.EqualTo(result));
     }
 
     [Test]
     public void Test_GetFruitQuantity_NullFruitName_ReturnsZero()
     {
-        // TODO: finish this test
+        // Arrange
+        Dictionary<string, int> fruitsDict = new()
+        {
+            { "apple" , 0 },
+            { "banana" , 0 },
+        };
+
+        string fruitName = null;
+        int expected = 0;
+
+        // Act 
+        int result = Fruits.GetFruitQuantity(fruitsDict, fruitName);
+
+
+        // Assert
+        Assert.That(expected, Is.EqualTo(result));
     }
 }
--- Exam-Preparation/2/03-ToDo/TestApp.Tests/ToDoListTests.cs
+++ Exam-Preparation/2/03-ToDo/TestApp.Tests/ToDoListTests.cs
@@ -1,5 +1,5 @@
-using System;
-
+﻿using System;
+using Microsoft.VisualBasic;
 using NUnit.Framework;
 
 using TestApp.Todo;
@@ -20,30 +20,74 @@ public class ToDoListTests
     [Test]
     public void Test_AddTask_TaskAddedToToDoList()
     {
-        // TODO: finish the test
+        // Arrange
+        string taskName = "Washing";
+        DateTime taskDate = DateTime.Today;
+        _toDoList.AddTask(taskName, taskDate);
+        //string dateString = "12/31/2023";
+        //DateTime taskDate = DateTime.Parse(dateString);
+
+
+        // Act 
+        string result = _toDoList.DisplayTasks();
+        //string expected = $"To-Do List:{Environment.NewLine}[ ] {taskName} - Due: {taskDate.ToString("MM/dd/yyyy")}";
+
+        // Assert
+        Assert.That(result, Does.Contain($"[ ] {taskName} - Due: "));
+        //Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_CompleteTask_TaskMarkedAsCompleted()
     {
-        // TODO: finish the test
+        // Arrange
+        string taskName = "task to complete";
+        DateTime taskDate = DateTime.Today;
+        _toDoList.AddTask(taskName, taskDate);
+        _toDoList.CompleteTask(taskName);
+
+
+        // Act 
+        string result = _toDoList.DisplayTasks();
+
+        // Assert
+        Assert.That(result, Does.Contain("[✓] task to complete - Due: "));
     }
 
     [Test]
     public void Test_CompleteTask_TaskNotFound_ThrowsArgumentException()
     {
-        // TODO: finish the test
+        // Arrange
+        string taskName = "task to complete";
+
+        // Act & Assert
+        Assert.That(() => _toDoList.CompleteTask(taskName), Throws.ArgumentException);
     }
 
     [Test]
     public void Test_DisplayTasks_NoTasks_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        // Act 
+        string result = _toDoList.DisplayTasks();
+        string expected = "To-Do List:";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_DisplayTasks_WithTasks_ReturnsFormattedToDoList()
     {
-        // TODO: finish the test
+        // Arrange
+        string taskName = "task to complete";
+        DateTime taskDate = DateTime.Today;
+        _toDoList.AddTask(taskName, taskDate);
+
+        // Act 
+        string result = _toDoList.DisplayTasks();
+        string expected = $"To-Do List:{Environment.NewLine}[ ] {taskName} - Due: {taskDate.ToString("MM/dd/yyyy")}";
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }

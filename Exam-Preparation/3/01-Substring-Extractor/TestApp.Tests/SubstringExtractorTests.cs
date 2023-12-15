b4064c70f75cfb204bece6e1d09a6d30b610a4a4
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-12-15 23:00:01 +0200

    exam prep

--- Exam-Preparation/3/01-Substring-Extractor/TestApp.Tests/SubstringExtractorTests.cs
+++ Exam-Preparation/3/01-Substring-Extractor/TestApp.Tests/SubstringExtractorTests.cs
@@ -8,36 +8,96 @@ public class SubstringExtractorTests
     [Test]
     public void Test_ExtractSubstringBetweenMarkers_SubstringFound_ReturnsExtractedSubstring()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "12abcdQWERTY!@#$%";
+        string startMarker = "QW";
+        string endMarker = "$%";
+        string expected = "ERTY!@#";
+
+        // Act
+        string result = SubstringExtractor.ExtractSubstringBetweenMarkers(input, startMarker, endMarker);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_ExtractSubstringBetweenMarkers_StartMarkerNotFound_ReturnsNotFoundMessage()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "12abcdQWERTY!@#$%";
+        string startMarker = "po";
+        string endMarker = "$%";
+        string expected = "Substring not found";
+
+        // Act
+        string result = SubstringExtractor.ExtractSubstringBetweenMarkers(input, startMarker, endMarker);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_ExtractSubstringBetweenMarkers_EndMarkerNotFound_ReturnsNotFoundMessage()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "12abcdQWERTY!@#$%";
+        string startMarker = "QW";
+        string endMarker = "po";
+        string expected = "Substring not found";
+
+        // Act
+        string result = SubstringExtractor.ExtractSubstringBetweenMarkers(input, startMarker, endMarker);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_ExtractSubstringBetweenMarkers_StartAndEndMarkersNotFound_ReturnsNotFoundMessage()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "12abcdQWERTY!@#$%";
+        string startMarker = "zx";
+        string endMarker = "po";
+        string expected = "Substring not found";
+
+        // Act
+        string result = SubstringExtractor.ExtractSubstringBetweenMarkers(input, startMarker, endMarker);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_ExtractSubstringBetweenMarkers_EmptyInput_ReturnsNotFoundMessage()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = string.Empty;
+        string startMarker = "zx";
+        string endMarker = "po";
+        string expected = "Substring not found";
+
+        // Act
+        string result = SubstringExtractor.ExtractSubstringBetweenMarkers(input, startMarker, endMarker);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_ExtractSubstringBetweenMarkers_StartAndEndMarkersOverlapping_ReturnsNotFoundMessage()
     {
-        // TODO: finish the test
+        // Arrange
+        string input = "12abcdQWERTY!@#$%";
+        string startMarker = "d";
+        string endMarker = "1";
+        string expected = "Substring not found";
+
+        // Act
+        string result = SubstringExtractor.ExtractSubstringBetweenMarkers(input, startMarker, endMarker);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }

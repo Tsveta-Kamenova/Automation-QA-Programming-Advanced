5b5b9122170c8b086d6daacdca70364708af6906
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-12-15 23:31:33 +0200

    exam prep

--- Exam-Preparation/3/02-Grades/TestApp.Tests/GradesTests.cs
+++ Exam-Preparation/3/02-Grades/TestApp.Tests/GradesTests.cs
@@ -11,24 +11,76 @@ public class GradesTests
     [Test]
     public void Test_GetBestStudents_ReturnsBestThreeStudents()
     {
-        // TODO: finish the test
+        // Arrange
+        Dictionary<string, int> gradesDict = new()
+        {
+            { "Ana", 5 },
+            { "Ivan", 2 },
+            { "Yasmina", 4 },
+            { "Hordu", 6 },            
+            { "Viktoria", 3 },
+            { "Marin", 8 },
+        };
+        string expected = $"Marin with average grade 8.00{Environment.NewLine}Hordu with average grade 6.00{Environment.NewLine}Ana with average grade 5.00";
+
+        // Act 
+        string result = Grades.GetBestStudents(gradesDict);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetBestStudents_EmptyGrades_ReturnsEmptyString()
     {
-        // TODO: finish the test
+        // Arrange
+        Dictionary<string, int> gradesDict = new();
+        string expected = string.Empty;
+
+        // Act 
+        string result = Grades.GetBestStudents(gradesDict);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetBestStudents_LessThanThreeStudents_ReturnsAllStudents()
     {
-        // TODO: finish the test
+        // Arrange
+        Dictionary<string, int> gradesDict = new()
+        {
+            { "Ana", 5 },
+            { "Ivan", 2 },
+        };
+        string expected = $"Ana with average grade 5.00{Environment.NewLine}Ivan with average grade 2.00";
+
+        // Act 
+        string result = Grades.GetBestStudents(gradesDict);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
     public void Test_GetBestStudents_SameGrade_ReturnsInAlphabeticalOrder()
     {
-        // TODO: finish the test
+        // Arrange
+        Dictionary<string, int> gradesDict = new()
+        {
+            { "Ivan", 2 },
+            { "Yasmina", 4 },
+            { "Hordu", 5 },
+            { "Viktoria", 3 },
+            { "Marin", 5 },
+            { "Ana", 5 },
+        };
+        string expected = $"Ana with average grade 5.00{Environment.NewLine}Hordu with average grade 5.00{Environment.NewLine}Marin with average grade 5.00";
+
+        // Act 
+        string result = Grades.GetBestStudents(gradesDict);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
     }
 }

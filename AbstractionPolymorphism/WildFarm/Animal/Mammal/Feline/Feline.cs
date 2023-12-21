17b9b898be4afcb66738098138d3c8e7f63a2561
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-12-21 17:18:31 +0200

    more

---
+++ AbstractionPolymorphism/WildFarm/Animal/Mammal/Feline/Feline.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WildFarm.Animal.Mammal.Feline
+{
+    public abstract class Feline : Mammal
+    {
+        public string Breed { get; set; }
+        protected Feline(string name, double weight, int foodEaten, string livingRegion, string breed) : base(name, weight, foodEaten, livingRegion)
+        {
+            Breed = breed;
+        }
+    }
+}

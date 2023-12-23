9792ba7bb8228d3dd73b38b36e5e1c9b6cf49d3e
Tsveta Kamenova <88488070+Tsveta-Kamenova@users.noreply.github.com>
2023-12-24 00:01:37 +0200

    same

--- AbstractionPolymorphism/WildFarm/Farm.cs
+++ AbstractionPolymorphism/WildFarm/Farm.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WildFarm.Animals;
 using WildFarm.Animals.Birds;
+using WildFarm.Animals.Mammals;
 using WildFarm.Animals.Mammals.Felines;
 using WildFarm.Foods;
 
@@ -65,10 +66,54 @@ namespace WildFarm
                 Hen hen = new(name, weight, wingSize);
                 AddAnimal(hen);
             }
+
+            else if (animalParams[0] == "Mouse")
+            {
+                string name = animalParams[1];
+                double weight = double.Parse(animalParams[2]);
+                string livingRegion = animalParams[3];
+
+                Mouse mouse = new(name, weight, livingRegion);
+                AddAnimal(mouse);
+            }
+
+            else if (animalParams[0] == "Dog")
+            {
+                string name = animalParams[1];
+                double weight = double.Parse(animalParams[2]);
+                string livingRegion = animalParams[3];
+
+                Dog dog = new(name, weight, livingRegion);
+                AddAnimal(dog);
+            }
         }
         public void FeedAnimal(Animal animal, Food food)
         {
-
+            if (animal.GetType().Name == "Hen")
+            {
+                animal.Weight += food.Quantity * 0.35;
+            }
+            else if (animal.GetType().Name == "Owl")
+            {
+                animal.Weight += food.Quantity * 0.25;
+            }
+            else if (animal.GetType().Name == "Mouse")
+            {
+                animal.Weight += food.Quantity * 0.10;
+            }
+            else if (animal.GetType().Name == "Cat")
+            {
+                animal.Weight += food.Quantity * 0.30;
+            }
+            else if (animal.GetType().Name == "Dog")
+            {
+                animal.Weight += food.Quantity * 0.40;
+            }
+            else if (animal.GetType().Name == "Tiger")
+            {
+                animal.Weight += food.Quantity * 1.00;
+            }
+            animal.FoodEaten += food.Quantity;
         }
 
 
